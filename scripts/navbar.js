0f5064049301dd8a46f0aae974e26819c4502bd8
Spudoku <calebisntshort@gmail.com>
2025-06-30 11:52:29 -0700

    working on a navbar
    30 minutes

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,3 +1,7 @@
+// This script provides all functionality for the navbar, including
+// darkmode, mouse hovering over certain elements, etc.
+
+// ====== darkmode implementation ======
 // Coloration variables
 let mainBackground = "#82859b";
 let mainBackgroundDark = "#3e4749";
@@ -17,12 +21,13 @@ let subContainerBackgroundDark = "#2f418f";
 // CSS variable references
 var r = document.querySelector(':root');
 
+const darkModeButton = document.getElementById("darkmode");
+
+const darkModeLabel = document.getElementById("darkmode-label");
+
 // set darkmode to true
 let isDarkMode = false;
 toggleDarkMode();
-
-
-const darkModeButton = document.getElementById("darkmode");
 
 darkModeButton.addEventListener("click", () => {
     toggleDarkMode();
@@ -38,11 +43,29 @@ function toggleDarkMode() {
         r.style.setProperty('--main-text-color', mainTextColorDark);
         r.style.setProperty('--container-background', containerBackgroundDark);
         r.style.setProperty('--sub-container-background', subContainerBackgroundDark);
+        darkModeLabel.textContent = "Dark";
     } else {
         r.style.setProperty('--main-background', mainBackground);
         r.style.setProperty('--heading-color', headingColor);
         r.style.setProperty('--main-text-color', mainTextColor);
         r.style.setProperty('--container-background', containerBackground);
         r.style.setProperty('--sub-container-background', subContainerBackground);
+        darkModeLabel.textContent = "Light";
     }
+}
+
+// ====== Navbar hovering functionality ======
+const navbar = document.getElementById("nav-area");
+
+navbar.addEventListener("mouseenter", (event) => { onMouseEnter(event.target) })
+navbar.addEventListener("mouseleave", (event) => { onMouseLeave(event.target) })
+
+function onMouseEnter(target) {
+    console.log(target);
+    target.style.backgroundColor = 'red';
+}
+
+function onMouseLeave(target) {
+    console.log(target);
+    target.style.backgroundColor = 'blue';
 }
